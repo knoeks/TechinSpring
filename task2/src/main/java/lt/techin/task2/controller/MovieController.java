3fa8eabe38fd6b7e2a553a12d46fa05327bdc5f6
Paul <povilas.geguzis@gmail.com>
2025-01-29 17:53:06 +0200

    s

--- task2/src/main/java/lt/techin/task2/controller/MovieController.java
+++ task2/src/main/java/lt/techin/task2/controller/MovieController.java
@@ -1,8 +1,10 @@
 package lt.techin.task2.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lt.techin.task2.model.Movie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MovieController {
-  private List<Movie> movies = new ArrayList<Movie>();
+
+  // String name, String rating, String genre
+  private final List<Movie> movies = new ArrayList<Movie>(
+          List.of(new Movie("juju", "18+", "fantasy"),
+                  new Movie("No Man", "13", "drama"),
+                  new Movie("Funny life", "8", "comedy"),
+                  new Movie("Fast and foodios", "18+", "thriller"),
+                  new Movie("Hobbit", "13", "fantasy")
+          )
+  );
+
+  @GetMapping("/movies")
+  public ResponseEntity<List<Movie>> getMovies() {
+    return ResponseEntity.ok(movies);
+  }
+
+  @GetMapping("/movies/{index}")
+  public ResponseEntity<Movie> getMovie(@PathVariable int index) {
+    if (index > movies.size() - 1) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(movies.get(index));
+  }
+
+  @PostMapping("/movies")
+  public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
+    if (movie.getGenre().isEmpty() || movie.getName().isEmpty() || movie.getRating().isEmpty()) {
+      return ResponseEntity.badRequest().build();
+    }
+
+    // cia galima papildyti ArrayList nes jau "tvarkingi" duomenys cia liks
+    movies.add(movie);
+
+    return ResponseEntity.created(
+                    ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{index}")
+                            .buildAndExpand(movies.size() - 1)
+                            .toUri())
+            .body(movie);
+  }
 
 }
--- task2/src/main/java/lt/techin/task2/model/Movie.java
+++ task2/src/main/java/lt/techin/task2/model/Movie.java
@@ -1,6 +1,37 @@
 package lt.techin.task2.model;
 
 public class Movie {
+  private String name;
+  private String rating;
+  private String genre;
 
+  public Movie(String name, String rating, String genre) {
+    this.name = name;
+    this.rating = rating;
+    this.genre = genre;
+  }
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getRating() {
+    return rating;
+  }
+
+  public void setRating(String rating) {
+    this.rating = rating;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
 }
